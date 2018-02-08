951d3e703810676c6847ae0911fea41b73f5832a
andy <andy2237@outlook.com>
2018-02-08 23:47:46 +0800

    修改bug（错误的注释了需要用到的变量，导致程序不能运行)

--- Task0.py
+++ Task0.py
@@ -19,24 +19,14 @@ with open('calls.csv', 'r') as f:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-# FMT_TEXT_STR = "First record of texts, {0} texts {1} at time {2}"
-# FMT_CALL_STR = "Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds"
-#
-# if len(texts) > 0:
-#     first_text_record = texts[0]
-#     print(FMT_TEXT_STR.format(first_text_record[0],
-#                               first_text_record[1],
-#                               first_text_record[2]))
-# else:
-#     print("no records found")
+FMT_TEXT_STR = "First record of texts, {0} texts {1} at time {2}"
+FMT_CALL_STR = "Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds"
 
-
-if len(calls) > 0:
-    last_call_record = calls[-1]
-    incoming_number = last_call_record[0]
-    answering_number = last_call_record[1]
-    action_time = last_call_record[2]
-    during_time = last_call_record[3]
-    print(FMT_CALL_STR.format(incoming_number, answering_number, action_time, during_time))
+if len(texts) > 0:
+    first_text_record = texts[0]
+    print(FMT_TEXT_STR.format(first_text_record[0],
+                              first_text_record[1],
+                              first_text_record[2]))
 else:
     print("no records found")
+
